e81310ef652e87550786a89c2ed86fb15848e3d9
Bhoomika Shetty <119508834+Bhoomikashetty26@users.noreply.github.com>
2025-02-03 11:39:08 +0530

    Delete internal  directory

--- internal /detection.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def detect_disease(image, model):
-    # Perform inference on the input image
-    results = model(image)
-    return results.xyxy[0].cpu().numpy()  # Get detection results as a numpy array
